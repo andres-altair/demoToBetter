af2f4d64aad5086259e865547c1105ba98ccc0b2
andres-altair <csi23-amoliaz@colegioaltair.net>
2026-01-13 14:21:23 +0100

    feat: estructura inicial del proyecto

---
+++ src/main/java/com/andres/demoToBetter/DemoToBetterApplication.java
@@ -0,0 +1,13 @@
+package com.andres.demoToBetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoToBetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoToBetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andres/demoToBetter/DemoToBetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.demoToBetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoToBetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
